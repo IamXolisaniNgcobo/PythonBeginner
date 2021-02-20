2a448ee24b64e029237be5b3608e348235dd7ff8
XOLISANI NGCOBO <38984822+Xolsann722@users.noreply.github.com>
2021-02-20 16:33:29 +0200

    initial push

---
+++ strings.py
@@ -0,0 +1,19 @@
+print("Today is a good day to learn python")
+
+print('python is amazing')
+
+print("Python's string are easy to use")
+
+print('We can even include "quotes" in  string')
+
+print("Hello " + "World!")
+
+greeting = "Hello"
+age = 29
+print(type(age))
+name = input("Please enter your name")
+
+print(greeting +' '+name)
+
+
+print("My name is " +name+" and i am ",age,"years old")
