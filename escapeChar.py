7bc1184fa5d1477083bcb127229df6ace72e7290
XOLISANI NGCOBO <38984822+Xolsann722@users.noreply.github.com>
2021-02-20 18:45:12 +0200

    escapeChar

---
+++ escapeChar.py
@@ -0,0 +1,20 @@
+splitString = "This string has been\nsplit over\nseveral\nlines" #new line \n
+print(splitString)
+
+anotherSplitString = """This string has been \
+split over \
+several \
+ines"""
+print(anotherSplitString)
+
+tabbedString = "1\t2\t3\t4\t5"  #tab space \t5
+print(tabbedString)
+
+print('The petshop owner said "No, no, \'e\'s uh,...he\'s resting".')   #print quotes inside a strings
+#OR
+print("""The petshop owner said "No, no, 'e's uh,...he's resting".""")
+#OR
+print("The petshop owner said \"No, no, 'e's uh,... he's resting\".")
+
+print(r"C:\Users\timbuchalka\notes.txt")
+print("C:\\Users\\timbuchalka\\notes.txt")
