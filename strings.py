ae3794d2136f94572f5d479a3a5410c8fec321fd
XOLISANI NGCOBO <38984822+Xolsann722@users.noreply.github.com>
2021-02-20 21:43:55 +0200

    Update strings.py

--- strings.py
+++ strings.py
@@ -9,7 +9,12 @@ print('We can even include "quotes" in  string')
 print("Hello " + "World!")
 
 greeting = "Hello"
+name = "Tim"
 age = 29
+print(name + f" is {age}  years old")   # fstrings
+print(f"Pi is approximately {22 / 7:12.50f}")
+pi = 22 / 7
+print(f"Pi is approximately {pi:12.50f}")
 print(type(age))
 name = input("Please enter your name")
 
