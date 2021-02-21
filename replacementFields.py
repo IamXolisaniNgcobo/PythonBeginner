33496368ae24d72dd0ea124c958d934515da45a9
XOLISANI NGCOBO <38984822+Xolsann722@users.noreply.github.com>
2021-02-21 16:56:38 +0200

    Update replacementFields.py

--- replacementFields.py
+++ replacementFields.py
@@ -1,7 +1,6 @@
 age = 24
 print("My age is " + str(age) + " years")
 print("My age is {0} years".format(age))
-
 print("There are {0} days in {1}, {2}, {3}, {4}, {5}, {6} and {7}"
       .format(31, "Jan", "Mar", "May", "Jul", "Aug", "Oct", "Dec")) # 31 to {0}...31, 0 is index containing 31
 
